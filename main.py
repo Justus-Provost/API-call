85aa79a3b42b41c99a2f14895d142159324a1e5b
Justus Provost <91761090+Justus-Provost@users.noreply.github.com>
2021-10-28 21:57:54 +0000

    poodle subbreeds listed

--- main.py
+++ main.py
@@ -7,4 +7,17 @@ query = ""
 url = url_host + endpoint + query
 
 response = requests.get(url)
-print(response.json())
+breeds = response.json()["message"]
+poodle = breeds.get("poodle")
+
+print(poodle)
+
+all_breeds = breeds.keys()
+
+for breed in all_breeds:
+  print(breed)
+
+poodle_subbreeds = breeds["poodle"]
+input("\nPress enter to get all poodle subbreeds")
+for sub in poodle_subbreeds:
+  print(sub)
